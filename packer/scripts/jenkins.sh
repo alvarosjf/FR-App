c68eb78e645b0526303b50756aade4f7e6ab187a
Alvaro <alvarosjf@gmail.com>
2020-06-14 19:29:17 +0200

    packer addition

---
+++ packer/scripts/jenkins.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install -y jenkins java git
+sudo chkconfig --add jenkins
+sudo systemctl start jenkins
